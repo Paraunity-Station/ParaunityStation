faffd36352d9cfb02bcacda4bb30d6716131a87b
NuclearWinter <nukeuler123@gmail.com>
2020-02-16 14:13:04 -0600

    Update to converter
    Simply updated the converter code, still now finished.
    A converted map file is now there for testing purposes (Who knew .dmm files were plaintext?).

--- Tools/Python .dmm to .unity converter/Converter.py
+++ Tools/Python .dmm to .unity converter/Converter.py
@@ -1,6 +1,26 @@
 import os
-U = open("Test.Unity", "x")
-f = open("Test.Unity", "a")
-c = open("Essential_Data", "r")
-f.write(c.read())
-f.close()
+# Changes the file extension so that the program can read it (not really needed but good to look over as a human)
+Fconv = False
+for file in os.listdir('.'):
+    for x in range(len(file) - 1):
+        if file[x-1:x] == ".":
+            if file[x:x+3] == "dmm":
+                os.rename(file, file[0:len(file)-3] + "txt")
+                mf = file[0:len(file)-3] + "txt"
+                Fconv = True
+                break
+# Creates the "base" of the map (The prefabs and managers needed to make a map work)
+
+if Fconv:
+    uname = mf[0:len(mf) - 4] + ".unity"
+    open(uname, "x")
+    w = open(uname, "a")
+    e = open("Essential_Data.txt", "r")
+    w.write(e.read())
+    w.close()
+else:
+    print("Error: No convertable .dmm files!")
+# Reads data from the new .txt file and interprets the tile data
+if Fconv:
+    dmd = open(mf, "r")
+    print(dmd.read())
