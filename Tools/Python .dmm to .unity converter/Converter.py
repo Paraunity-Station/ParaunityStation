d80b5d45b6f6c18ae4f09cf7b15e763039e13825
NuclearWinter <nukeuler123@gmail.com>
2020-02-17 19:04:18 -0600

    Converter update
    Still not finished but it can see the space tiles

--- Tools/Python .dmm to .unity converter/Converter.py
+++ Tools/Python .dmm to .unity converter/Converter.py
@@ -1,26 +1,34 @@
 import os
+
 # Changes the file extension so that the program can read it (not really needed but good to look over as a human)
-Fconv = False
+Fconv = True  # Change to False later
 for file in os.listdir('.'):
     for x in range(len(file) - 1):
-        if file[x-1:x] == ".":
-            if file[x:x+3] == "dmm":
-                os.rename(file, file[0:len(file)-3] + "txt")
-                mf = file[0:len(file)-3] + "txt"
+        if file[x - 1:x] == ".":
+            if file[x:x + 3] == "dmm":
+                os.rename(file, file[0:len(file) - 3] + "txt")
+                mf = file[0:len(file) - 3] + "txt"
                 Fconv = True
                 break
+# Dev bypass, this removes the need for the conversion process to allow rapid testing of code
+mf = "cyberiad.txt"
 # Creates the "base" of the map (The prefabs and managers needed to make a map work)
 
 if Fconv:
     uname = mf[0:len(mf) - 4] + ".unity"
     open(uname, "x")
     w = open(uname, "a")
-    e = open("Essential_Data.txt", "r")
+    e = open("Base_Data.txt", "r")
     w.write(e.read())
     w.close()
 else:
     print("Error: No convertable .dmm files!")
 # Reads data from the new .txt file and interprets the tile data
 if Fconv:
-    dmd = open(mf, "r")
-    print(dmd.read())
+    fd = open(mf, "r")
+    fd = fd.read()
+    for x in range(len(fd) - 1):
+        if fd[x:x + 1] == "a":
+            if fd[x:x + 2] == "aa":
+                pass
+# need help automatically setting defines for tiles
