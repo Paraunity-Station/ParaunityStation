d081b63d92c4d6daf2a17f2aa18a2b29e159f5a0
NuclearWinter <nukeuler123@gmail.com>
2020-02-29 11:01:09 -0600

    Synced fork to origin and moved files
    Moved the body sprites to their respective bodyparts folders while updating the fork to the origin

---
+++ Tests/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public int fstnum = 2;
+	public int sndnum = 3;
+	void Start()
+    {
+		addnum();
+	}
+
+	void addnum()
+	{
+		Debug.Log(fstnum + sndnum);
+	}
+
+
+
+
+}
+
+
+
